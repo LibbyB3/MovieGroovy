2a361c6c0f6e95bbc119ea37cf077bcaa4f4615a
LBPilots51 <libbyblair13@gmail.com>
2023-07-14 14:11:07 -0500

    Deleting old models and creating a UsersModel.

---
+++ MovieAppCSC660/Models/UsersModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLibrary.Models
+{
+    public class UsersModel
+    {
+        public int UserID { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public UsersModel(string usernameTextBox, string emailTextBox, string passwordTextBox)
+        {
+            Username = usernameTextBox;
+            Email = emailTextBox;
+            Password = passwordTextBox;
+        }
+    }
+}
